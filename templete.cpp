b8030025f478928d5e575903bf910dab6b50d521
Shivang Mangal <shivangmangal@gmail.com>
2022-06-22 19:46:19 +0530

    Initial commit

---
+++ templete.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#define int long long
+#define pb push_back
+#define mp make_pair
+#define mod 1000000007
+#define INF (int)1e9
+#define PI 3.1415926535897932384626433832795
+#define MEM(a, b) memset(a, (b), sizeof(a))
+using namespace std;
+void solve()
+{
+    
+}
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        solve();
+    }
+}
